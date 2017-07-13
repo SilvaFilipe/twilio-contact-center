60f941c557a201f376fcb3db11419f1924ecbf3d
SilvaFilipe <silva.filipef@gmail.com>
2017-07-13 16:30:29 +0100

    1630

--- controllers/ivr.js
+++ controllers/ivr.js
@@ -18,12 +18,17 @@ module.exports.welcome = function (req, res) {
 		keywords.push(req.configuration.ivr.options[i].friendlyName)
 	}
 
+	const response = new twilio();
+response.say({
+    language: 'sv-SE'
+}, 'Hej!');
+
 	twiml.gather({
-		/*input: 'dtmf speech',
+		input: 'dtmf speech',
 		action: 'select-team',
 		method: 'GET',
 		numDigits: 1,
-		timeout: 4,*/
+		timeout: 4,
 		voice: 'alice',
 		language: 'fr-FR',
 		hints: keywords.join()
