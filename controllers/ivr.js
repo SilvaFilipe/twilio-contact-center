ded21f07daa2e7d0f5e222f22ecdfdfd29d87656
SilvaFilipe <silva.filipef@gmail.com>
2017-07-14 15:05:51 +0100

    1505

--- controllers/ivr.js
+++ controllers/ivr.js
@@ -77,7 +77,7 @@ module.exports.selectTeam = function (req, res) {
 	/* the caller pressed a key that does not match any team */
 	if (team === null) {
 		// redirect the call to the previous twiml
-		twiml.play("https://www.dropbox.com/s/76a171hot9e3iba/selecao_invalida.wav")
+		twiml.play("https://www.dropbox.com/s/dps8ll5z229q6ca/selecao_invalida.mp3")
 		twiml.play('Your selection was not valid, please try again')
 		twiml.pause({length: 2})
 		twiml.redirect({ method: 'GET' }, 'welcome')
