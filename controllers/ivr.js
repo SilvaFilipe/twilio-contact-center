21cf22835e2b45708c703be825a2948f9653362f
SilvaFilipe <silva.filipef@gmail.com>
2017-07-21 15:22:02 +0100

    teste2

--- controllers/ivr.js
+++ controllers/ivr.js
@@ -26,8 +26,11 @@ module.exports.welcome = function (req, res) {
 		hints: keywords.join()
 	}, function (node) {
 		node.say(req.configuration.ivr.text)
-		if(req.configuration.ivr.options[i].friendlyName=='Sales'){
+
+		if(req.configuration.ivr.options[i].friendlyName == 'Sales'){
 			twiml.play("https://secure2.domdigital.pt/domdigital/micael/selecao_invalida.mp3")
+		}else {
+			twiml.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
 		}
 
 		/*twiml.play("https://secure2.domdigital.pt/domdigital/micael/marque_numero.mp3")
