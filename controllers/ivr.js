f53b03309f4ac3aaf28083e866858b846963e345
SilvaFilipe <silva.filipef@gmail.com>
2017-07-26 10:51:55 +0100

    1051

--- controllers/ivr.js
+++ controllers/ivr.js
@@ -54,7 +54,8 @@ module.exports.welcome = function (req, res) {
 				
 		}
 
-	twiml.say('You did not say anything or enter any digits.')
+	//twiml.say('You did not say anything or enter any digits.')
+	twiml.play("https://secure2.domdigital.pt/domdigital/micael/mp3/nao_marcou.mp3")
 	twiml.pause({length: 1})
 	twiml.redirect({method: 'GET'}, 'welcome')
 
@@ -101,7 +102,8 @@ module.exports.selectTeam = function (req, res) {
 	/* the caller pressed a key that does not match any team */
 	if (team === null) {
 		// redirect the call to the previous twiml
-		twiml.say('Your selection was not valid, please try again')
+		//twiml.say('Your selection was not valid, please try again')
+		twiml.play("https://secure2.domdigital.pt/domdigital/micael/mp3/selecao_invalida.mp3")
 		twiml.pause({length: 2})
 		twiml.redirect({ method: 'GET' }, 'welcome')
 	} else {
@@ -113,7 +115,17 @@ module.exports.selectTeam = function (req, res) {
 			timeout: 5
 		})
 
-		gather.say('Press a key if you want a callback from ' + team.friendlyName + ', or stay on the line')
+		//gather.say('Press a key if you want a callback from ' + team.friendlyName + ', or stay on the line')
+			gather.play("https://secure2.domdigital.pt/domdigital/micael/mp3/pressione_volta.mp3")
+			if(team.friendlyName=='Sales'){
+					gather.play("https://secure2.domdigital.pt/domdigital/micael/mp3/vendas.mp3")
+				}else if(team.friendlyName=='Support'){
+					gather.play("https://secure2.domdigital.pt/domdigital/micael/mp3/suporte.mp3")
+				}else if(team.friendlyName=='Marketing'){
+					gather.play("https://secure2.domdigital.pt/domdigital/micael/mp3/marketing.mp3")
+				}
+			
+			gather.play("https://secure2.domdigital.pt/domdigital/micael/mp3/continue.mp3")
 
 		/* create task attributes */
 		const attributes = {
@@ -151,10 +163,12 @@ module.exports.createTask = function (req, res) {
 
 	taskrouterHelper.createTask(req.configuration.twilio.workflowSid, attributes)
 		.then(task => {
-			twiml.say('Thanks for your callback request, an agent will call you back soon.')
+			//twiml.say('Thanks for your callback request, an agent will call you back soon.')
+			twiml.play("https://secure2.domdigital.pt/domdigital/micael/mp3/obrigado_pedido.mp3")
 			twiml.hangup()
 		}).catch(error => {
-			twiml.say('An application error occured, the demo ends now')
+			//twiml.say('An application error occured, the demo ends now')
+			twiml.play("https://secure2.domdigital.pt/domdigital/micael/mp3/erro.mp3")
 		}).then(() => {
 			res.send(twiml.toString())
 		})
